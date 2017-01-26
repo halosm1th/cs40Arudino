801a5fe820b5a0710a804aa2feb13d4f33156f9a
haloguysm1th <thomastrkie@gmail.com>
2017-01-26 09:08:15 -0600

    Updated some code

--- src/roombaCode/roombaCode.ino
+++ src/roombaCode/roombaCode.ino
@@ -7,14 +7,12 @@ int ddPin = 5;
 SoftwareSerial mySerial(rxPin, txPin);
 
 void setup() {
-
-  Serial.begin(9600);
+  Serial.begin(19200);
   delay(1000);
   Serial.println("Hello Dave");
-  // Serial.print("Hello Dave");
-
   pinMode (ddPin, OUTPUT);
   Serial.println("Hello Dave, pinmod set");
+    delay(1000);
   mySerial.begin(19200);
   Serial.println("Hello Dave, serial began");
   delay(1000);
@@ -22,39 +20,54 @@ void setup() {
   if (mySerial.available())
     Serial.println(mySerial.read());
   else
-    Serial.println("test"
-                  );
+    Serial.println("test");
   Serial.println("Hello Dave, we read from serial!");
-/*
   digitalWrite(ddPin, HIGH);
   delay(100);
   digitalWrite(ddPin, LOW);
   delay(500);
   digitalWrite(ddPin, HIGH);
-  */
   delay(2000);
   Serial.println("I'm awake");
-  //mySerial.write(128);
-  //delay(10);
-  Serial.println("Going into safemode");
+   delay(1000);
+  mySerial.write(128);
+  delay(1000);
   mySerial.write(131);
-  delay(100);
-/*
-  Serial.println("Coding the song");
+  delay(1000);
+  Serial.println("Going into safemode");
+  delay(1000);
   codeSong();
-  Serial.println("Writting the song");
-  swrite(141);
+   swrite(141);
   Serial.println("Writing to play");
   swrite(1);
-  
-  mySerial.write(137);
-  sendHighLow(-200);
-  sendHighLow(500);
-
+  delay(1000);
+    for(int i = 0; i < 10; i++){
+        mySerial.write(137);
+  sendHighLow(50);
+  sendHighLow(25);
+  driveStraight(100);
+  driveStraight(0);
+  }
   endofCode();
-  /* */
-}
 
+}
+void driveStraight(int vel){
+  mySerial.write(137);
+  sendHighLow(vel);
+  mySerial.write(128);
+  mySerial.write(1);
+}
+void drive(int vel, int rad){
+  mySerial.write(137);
+  sendHighLow(vel);
+  sendHighLow(rad);k
+}
+void sendHighLow(int send) {
+  mySerial.write(highByte(send));
+  delay(100);
+  mySerial.write(lowByte(send));
+  delay(100);
+}
 void codeSong(){
   Serial.println("Wrote opcode for writing song");
   swrite(140);
@@ -63,22 +76,9 @@ void codeSong(){
   Serial.println("WRiting song len");
   swrite(16);
   Serial.println("Wriring notes");
-  playNote(31,32);
-  playNote(33,32);
-  playNote(35,32);
-  playNote(38,32);
-  playNote(40,32);
-  playNote(41,32);
-  playNote(43,32);
-  playNote(47,32);
-  playNote(48,32);
-  playNote(50,32);
-  playNote(52,32);
-  playNote(53,32);
-  playNote(55,32);
-  playNote(57,32);
-  playNote(59,32);
-  playNote(60,32);
+  for(int i = 64; i < 80; i++){
+    playNote(i,255);
+  }
 }
 
 void playNote(int note, int len){
@@ -91,21 +91,13 @@ void swrite(int message){
   delay(100);
 }
 
-void endofCode(){
+ void endofCode(){
   mySerial.write(143);
-  delay(100);
-  mySerial.write(173);
-  delay(100);
-}
-
-void sendHighLow(int send) {
-  mySerial.write(highByte(send));
-  delay(100);
-  mySerial.write(lowByte(send));
   delay(100);
 }
 
 void loop() {
   // Serial.println("Hello Dave");
+
 }
 
